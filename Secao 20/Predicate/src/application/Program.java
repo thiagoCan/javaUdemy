3f0a42815af4a4e101a6366b926f52a99680b17a
Thiago Fernandes Canonici <t_canonici@hotmail.com>
2022-06-14 22:46:43 -0300

    Expresao lambada inline

--- Secao 20/Predicate/src/application/Program.java
+++ Secao 20/Predicate/src/application/Program.java
@@ -21,11 +21,8 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		double min = 100.00;
-		
-		Predicate<Product> pred = p -> p.getPrice() >= min;
-		
-		
-		list.removeIf(pred);
+	
+		list.removeIf(p -> p.getPrice() >= min);
 		
 		for (Product p : list) {
 			System.out.println(p);
