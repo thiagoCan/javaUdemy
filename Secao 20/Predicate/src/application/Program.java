03e4ac6d44a404ef35fcc62f5b7bc253f3eec403
Thiago Fernandes Canonici <t_canonici@hotmail.com>
2022-06-14 22:30:21 -0300

    Expressao lambda declarada

--- Secao 20/Predicate/src/application/Program.java
+++ Secao 20/Predicate/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -19,8 +20,12 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		//Referência para o método
-		list.removeIf(Product::nonStaticProductProdicate);
+		double min = 100.00;
+		
+		Predicate<Product> pred = p -> p.getPrice() >= min;
+		
+		
+		list.removeIf(pred);
 		
 		for (Product p : list) {
 			System.out.println(p);
